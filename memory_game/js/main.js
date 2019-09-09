4007a63b9a7ebf2950b4aa4f6936c1c7a1b522a1
Ian Livingston <ian@Ians-MacBook-Pro.local>
2019-09-09 10:22:25 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,25 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+//arrays
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+//variables
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 
 console.log("User Flipped " + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User Flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[3]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
+
+
